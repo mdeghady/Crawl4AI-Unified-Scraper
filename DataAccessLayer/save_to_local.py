d05fb1b39fbdbf04e0fe0b23521cd137482431bd
Mostafa Deghady <mdeghady>
2025-05-11 12:27:45 +0200

    adding file path , folder path and ifnoring partitioning columns when reading parquet'

--- DataAccessLayer/save_to_local.py
+++ DataAccessLayer/save_to_local.py
@@ -3,29 +3,34 @@ import pyarrow.parquet as pq
 import os
 from datetime import datetime
 from threading import Lock
-from typing import List, Dict
+from typing import List, Dict, Tuple
 
 
 class ProductDataAccessLayer:
-    def __init__(self, website:str,output_dir: str = ".data/products"):
+    def __init__(self, website:str,output_dir: str = "data/"):
         self.output_dir = output_dir
         self.lock = Lock()  # Thread-safe file operations
         self.website = website
         os.makedirs(self.output_dir, exist_ok=True)
 
-    def _get_partitioned_path(self) -> str:
+    def _get_partitioned_path(self) -> Tuple[str, str]:
         year_part = datetime.now().strftime("%Y")
         month_part = datetime.now().strftime("%m")
         day_part = datetime.now().strftime("%d")
-        timestamp_part = datetime.now().strftime("%H-%M-%S")
+        timestamp_part = datetime.now().strftime("%H")
         website_part = self.website.replace(" ", "_").lower()
         current_path = os.path.dirname(os.path.abspath(__name__))
-        return os.path.join(
+        folder_path = os.path.join(
             current_path,
             self.output_dir,
             'year={}/month={}/day={}/website={}'.format(year_part, month_part, day_part, website_part),
-            f"products_{timestamp_part}.parquet"
         )
+        file_path = os.path.join(folder_path , f"products_{timestamp_part}.parquet")
+
+        # Create directory if it doesn't exist
+        os.makedirs(folder_path, exist_ok=True)
+
+        return folder_path , file_path
 
     def save_product(self, product: List[Dict]) -> None:
         """Save a single product to Parquet"""
@@ -34,14 +39,16 @@ class ProductDataAccessLayer:
             df = pd.DataFrame(product)
 
             # Define file path
-            file_path = self._get_partitioned_path()
-
-            # Create directory if it doesn't exist
-            os.makedirs(file_path, exist_ok=True)
+            folder_path , file_path = self._get_partitioned_path()
 
             # Append or create new file
             if os.path.exists(file_path):
                 existing = pq.read_table(file_path).to_pandas()
+
+                # Choose only columns that are in the new DataFrame
+                existing = existing[df.columns]
+
+                # Concatenate existing and new DataFrame
                 df = pd.concat([existing, df], ignore_index=True)
 
             # Save with compression
