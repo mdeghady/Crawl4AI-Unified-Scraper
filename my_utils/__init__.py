d2ec19616e995ed4211af467bacc7e995bbc1b05
Mostafa Deghady <mdeghady>
2025-05-28 14:31:31 +0200

    Added cleanning sizes and prices

--- my_utils/__init__.py
+++ my_utils/__init__.py
@@ -1,4 +1,4 @@
 from .full_back_strategy import FallbackJsonCssExtractionStrategy
+from .data_cleanser import DataCleasnerUtils
 
-
-__all__ = ['FallbackJsonCssExtractionStrategy']
+__all__ = ['FallbackJsonCssExtractionStrategy' , 'DataCleasnerUtils']
--- my_utils/data_cleanser.py
+++ my_utils/data_cleanser.py
@@ -23,3 +23,28 @@ class DataCleasnerUtils:
         # Matches common currency symbols (including crypto and rare ones)
         pattern = r'[€$£¥₹₽₩₪₺₴₸₿฿₫៛₡₱₲₵₶₾₼₠₣₤₥₦₧₨₩₪₫₭₮₯₰₳₷₺₻₼₽₾₿]'
         return re.findall(pattern, text)[0]
+
+    def convert_price_to_float(self , price_string,delimiter):
+        """Convert price string to float
+        :arg
+            price_string: string e.g. '€75.5', '60$', '3£'
+            delimiter: string e.g. '.', ','
+        :return
+            float: e.g. 75.5, 60.0, 3.0
+        """
+        if not price_string:
+            return 0.0
+        price_string = re.sub(r'[^\d{}]'.format(delimiter), '', price_string)
+        return float(price_string.replace(delimiter, '.')) if price_string else 0.0
+
+    def flatten_list_of_dicts(self, list_of_dicts , dicts_key):
+        """Flatten a list of dictionaries into a single list of values
+        :arg
+            list_of_dicts: list of dictionaries e.g. [{'Size': 'S'}, {'Size': 'M'}, {'Size': 'L'}]
+            dicts_key: key to extract from each dictionary e.g. 'Size'
+        :return
+            flattened_dict: list of values e.g. ['S', 'M', 'L']
+        """
+        flat_list = [d[dicts_key] for d in list_of_dicts if dicts_key in d]
+
+        return flat_list
